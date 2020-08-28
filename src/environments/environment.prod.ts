02f4345ce18d253db1833a91c55c7f927815d14e
vadzim84 <vkaraniec@gmail.com>
2020-08-28 16:53:05 +0300

    feat(add environment.prod)
    added environment.prod

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBFpCfEAAT-ltA-MqDdgTMOcr877v2gLlc",
+    authDomain: "treba-zrabic.firebaseapp.com",
+    databaseURL: "https://treba-zrabic.firebaseio.com",
+    projectId: "treba-zrabic",
+    storageBucket: "treba-zrabic.appspot.com",
+    messagingSenderId: "666009038556",
+    appId: "1:666009038556:web:3b0f041061a1ec20498e19"
+  }
+};
