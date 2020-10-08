46e0b3cfae7b98c4ff9f26eadff5a13bdc32a68b
vadzim84 <vkaraniec@gmail.com>
2020-10-08 14:01:30 +0300

    feat(tests)

--- src/app/auth/auth-service/auth.service.spec.ts
+++ src/app/auth/auth-service/auth.service.spec.ts
@@ -1,16 +1,27 @@
-import { TestBed } from '@angular/core/testing';
-
+import { inject, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { AuthService } from './auth.service';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [AuthService]
+    });
     service = TestBed.inject(AuthService);
   });
 
-  it('should be created', () => {
+  it('should be created', inject([AuthService], (service: AuthService) => {
     expect(service).toBeTruthy();
-  });
+  }));
+
+  // it('should get user', inject([AuthService, HttpTestingController],
+  //   (service: AuthService, beckend: HttpClientTestingModule) => {
+  //   expect(service).toBeTruthy();
+  // }));
+
+
+
 });
--- src/app/content/task/task.component.spec.ts
+++ src/app/content/task/task.component.spec.ts
@@ -1,14 +1,34 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute, Router } from '@angular/router';
+import { of } from 'rxjs';
+import { TaskService } from 'src/app/shared/services/task-service/task.service';
 
 import { TaskComponent } from './task.component';
 
 describe('TaskComponent', () => {
   let component: TaskComponent;
   let fixture: ComponentFixture<TaskComponent>;
+  const TASK_SERVICE_MOCK = {
+    getTaskId: () => true
+  }
+  const ROUTER_MOCK = {}
+  const ACTIVATED_ROUTE_MOCK = {
+    paramMap: of({
+      has: () => true,
+      get: () => 1,
+      getAll: () => [1, 2, 3],
+    })
+  }
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TaskComponent ]
+      declarations: [ TaskComponent ],
+      providers: [
+        {provide: TaskService, usValue: TASK_SERVICE_MOCK},
+        {provide: Router, usValue: ROUTER_MOCK},
+        {provide: ActivatedRoute, usValue: ACTIVATED_ROUTE_MOCK},
+      ]
+
     })
     .compileComponents();
   }));
@@ -22,4 +42,16 @@ describe('TaskComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+
+  it('onInit should be work', () => {
+    component.ngOnInit();
+    expect(component.taskId).toBe('1')
+
+
+
+    // expect(component).toBeTruthy();
+  });
+
+
 });
